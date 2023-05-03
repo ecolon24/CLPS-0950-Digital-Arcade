49de3afd2642aa5a70f7c6e21b78f3a4939134b0
ecolon24 <elicia_colon@brown.edu>
2023-05-03 11:13:51 -0400

    making the s shape configuration(s)

--- Tetris.py
+++ Tetris.py
@@ -8,7 +8,20 @@ play_width=300
 play_height= 600
 block_size= 30
 
+#play area
 upper_left_x= (screen_width-play_width)//2
 upper_left_y= screen_height - play_height
 
-#Making the shapes of the blocks 
+#Making the shapes of the blocks; accounting for different rotations+positioning
+S_shape= [['.....'
+           '.....'
+           '..00.'
+           '.00..'
+           '.....'],
+           ['.....'
+            '..0..'
+            '..00.'
+            '...0.'
+            '.....'
+            ]]
+print(S_shape)
