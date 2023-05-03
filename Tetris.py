12b6c10b78fe81c47a566dba7ea6c7da99f77ab0
ecolon24 <elicia_colon@brown.edu>
2023-05-03 12:21:01 -0400

    Shapes and Shape Colors!
    drew out all the orientations on Ipad then coded the in; lists within lists! researched colors+assigned to dif. tetrominos

--- Tetris.py
+++ Tetris.py
@@ -13,6 +13,8 @@ upper_left_x= (screen_width-play_width)//2
 upper_left_y= screen_height - play_height
 
 #Making the shapes of the blocks; accounting for different rotations+positioning
+#0 will represent where the block actually is placed, 5X5 setup!
+#have a notability file on Ipad that draws out all the different orientations; took some time!
 S_shape= [['.....'
            '.....'
            '..00.'
@@ -24,4 +26,96 @@ S_shape= [['.....'
             '...0.'
             '.....'
             ]]
-print(S_shape)
+
+Z_shape= [['.....'
+           '.....'
+           '.00..'
+           '..00.'
+           '.....'],
+           ['.....'
+            '..0..'
+            '.00..'
+            '.0...'
+            '.....']]
+I_shape= [['..0..'
+           '..0..'
+           '..0..'
+           '..0..'
+           '.....'],
+           ['.....'
+            '0000.'
+            '.....'
+            '.....'
+            '.....']]
+O_shape=[['.....'
+          '.....'
+          '.00..'
+          '.00..'
+          '.....']]
+#O_shape only has one configuration because a square when rotated still has the same shape!
+J_shape= [['.....'
+           '.0...'
+           '.000.'
+           '.....'
+           '.....'],
+           ['.....'
+            '.....'
+            '.000.'
+            '...0.'
+            '.....'],
+            ['.....'
+             '..0..'
+             '..0..'
+             '.00..'
+             '.....'],
+             ['.....'
+              '..00.'
+              '..0..'
+              '..0..'
+              '.....']]
+
+L_shape= [['.....'
+           '...0.'
+           '.000.'
+           '.....'
+           '.....'],
+           ['.....'
+            '..0..'
+            '..0..'
+            '..00.'],
+            ['.....'
+             '.....'
+             '.000.'
+             '.0...'
+             '.....'],
+             ['.....'
+              '.00..'
+              '..0..'
+              '..0..'
+              '.....']]
+T_shape= [['.....'
+           '..0..'
+           '.000.'
+           '.....'
+           '.....'],
+           ['.....'
+            '..0..'
+            '..00.'
+            '..0..'
+            '.....'],
+            ['.....'
+             '..0..'
+             '.00..'
+             '..0..'
+             '.....'],
+             ['.....'
+              '.....'
+              '.000.'
+              '..0..'
+              '.....']]
+
+#Shape Colors! Had to do a bit of research to find these but :) 
+#fun fact: Avid tetris fans refer to 'Tetris shapes' as tetriminos!
+tetriminos= [S_shape, Z_shape, I_shape, O_shape, J_shape, L_shape, T_shape]
+tetrimino_colors= [(0,255,0), (255,0,0), (0,255,255),(255,255,0),(225,165,0),(0,0,255),(128,0,128)]
+print(tetriminos)
