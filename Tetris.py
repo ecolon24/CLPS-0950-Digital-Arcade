3bd0fbc5363fad026f572abbc2eb3081834e4055
ecolon24 <elicia_colon@brown.edu>
2023-05-03 11:04:56 -0400

    Tetris; Global Variables + Playing Field Outline

---
+++ Tetris.py
@@ -0,0 +1,14 @@
+import pygame
+import random 
+
+#Global Variables 
+screen_width= 800
+screen_height= 700 
+play_width=300
+play_height= 600
+block_size= 30
+
+upper_left_x= (screen_width-play_width)//2
+upper_left_y= screen_height - play_height
+
+#Making the shapes of the blocks 
