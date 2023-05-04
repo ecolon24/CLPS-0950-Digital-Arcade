58ef6c6ec299901dafbf9eba82253dd2d31b44b9
rowenex <rowen_lee@brown.edu>
2023-05-04 14:04:45 -0400

    Trying to figure out moving the "snake"

--- snake.py
+++ snake.py
@@ -12,11 +12,18 @@ black = (0,0,0)
 textfont = pygame.font.SysFont('Arial',18)
 quittext = textfont.render('Quit' , True , black)
 
+square_size = 50
+square_x = width // 2 - square_size // 2
+square_y = height // 2 - square_size // 2
+square_color = (255, 0, 0)
+
+
 #creating the window
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Digital Aracde')
 screen.fill(background_colour)
 pygame.display.flip()
+
 
 def snake():
     running = True
@@ -32,10 +39,22 @@ def snake():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            elif event.type == pygame.MOUSEBUTTONDOWN:
                 if width-280 <= mouse[0] <= width-140 and height-80 <= mouse[1] <= height-40:
                     pygame.quit()
                     sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    square_x -= 5
+                elif event.key == pygame.K_RIGHT:
+                    square_x += 5
+                elif event.key == pygame.K_UP:
+                    square_y -= 5
+                elif event.key == pygame.K_DOWN:
+                    square_y += 5
+
+        pygame.draw.rect(screen, square_color, (square_x, square_y, square_size, square_size))
+        pygame.display.flip()
 
 
 snake()
