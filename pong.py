28315fa7651867feb4c4a1d102e2172e9707af02
eviekr <evelyn_krall@brown.edu>
2023-05-08 11:03:33 -0400

    getting two players to run

--- pong.py
+++ pong.py
@@ -2,89 +2,6 @@ import pygame
 import sys
 def game():
     pygame.init()
-
-    
-    #background_colour = (204,229,255)
-    button_light = (170,170,170)
-    button_dark = (140,140,140)
-    black = (0,0,0)
-    background = pygame.image.load("Documents/GitHub/CLPS-0950-Digital-Arcade/pongbackground.png")
-
-
-    #dimensions of window
-    (width, height) = (960, 720)
-
-    #creating the window
-    screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption('Pong Question Screen')
-    screen.blit(background, (0,0))
-    pygame.display.flip()
-
-    #text set up 
-    textfont = pygame.font.SysFont('Arial',16)
-    quittext = textfont.render('Quit' , True , black)
-    onetext = textfont.render('One Player' , True , black)
-    twotext = textfont.render('Two Players' , True , black)
-
-
-
-    running = True
-    while running: 
-        mouse = pygame.mouse.get_pos()
-    #code for making one player button lighter when hovered over it 
-        if width/2-280 <= mouse[0] <= width/2-140 and height/2-60 <= mouse[1] <= height/2-20:
-            pygame.draw.rect(screen,button_light,[width/2-280,height/2-60,140,40])    
-        else:
-            pygame.draw.rect(screen,button_dark,[width/2-280,height/2-60,140,40])
-    #code for making two player button lighter when hovered over it 
-        if width/2+140 <= mouse[0] <= width/2+280 and height/2-60 <= mouse[1] <= height/2-20:
-            pygame.draw.rect(screen,button_light,[width/2+140,height/2-60,140,40])    
-        else:
-            pygame.draw.rect(screen,button_dark,[width/2+140,height/2-60,140,40])
-    #code for making quit button lighter when hovered over it 
-        if width/2-70 <= mouse[0] <= width/2+70 and height/2+20 <= mouse[1] <= height/2+60:
-            pygame.draw.rect(screen,button_light,[width/2-70,height/2+20,140,40])        
-        else:
-            pygame.draw.rect(screen,button_dark,[width/2-70,height/2+20,140,40])
-      
-        screen.blit(quittext , (width/2-20,height/2+30))
-        screen.blit(onetext , (width/2-250,height/2-50))
-        screen.blit(twotext , (width/2+170,height/2-50))
-        pygame.display.update()  
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if width/2-70 <= mouse[0] <= width/2+70 and height/2+20 <= mouse[1] <= height/2+60:
-                    pygame.quit()
-                    sys.exit()
-            #What happens when the tetris button is clicked
-                if width/2-280 <= mouse[0] <= width/2-140 and height/2-60 <= mouse[1] <= height/2-20:
-                    running = False
-                    twoplayer()
-            #What happens when the space invaders button is clicked
-                if width/2+140 <= mouse[0] <= width/2+280 and height/2-60 <= mouse[1] <= height/2-20:
-                    running = False
-                    twoplayer()
-                    
-                  
-        
-        mouse = pygame.mouse.get_pos()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if width-280 <= mouse[0] <= width-140 and height-80 <= mouse[1] <= height-40:
-                    pygame.quit()
-                    sys.exit()
-
-
-
-
-
     def twoplayer():
         wwidth, wheight = 700, 500
         WIN = pygame.display.set_mode((wwidth, wheight))
@@ -270,6 +187,87 @@ def game():
                 right_score = 0
 
         pygame.quit()
+
+    
+    #background_colour = (204,229,255)
+    button_light = (170,170,170)
+    button_dark = (140,140,140)
+    black = (0,0,0)
+    background = pygame.image.load("Documents/GitHub/CLPS-0950-Digital-Arcade/pongbackground.png")
+
+
+    #dimensions of window
+    (width, height) = (960, 720)
+
+    #creating the window
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption('Pong Question Screen')
+    screen.blit(background, (0,0))
+    pygame.display.flip()
+
+    #text set up 
+    textfont = pygame.font.SysFont('Arial',16)
+    quittext = textfont.render('Quit' , True , black)
+    onetext = textfont.render('One Player' , True , black)
+    twotext = textfont.render('Two Players' , True , black)
+
+
+
+    running = True
+    while running: 
+        mouse = pygame.mouse.get_pos()
+    #code for making one player button lighter when hovered over it 
+        if width/2-280 <= mouse[0] <= width/2-140 and height/2-60 <= mouse[1] <= height/2-20:
+            pygame.draw.rect(screen,button_light,[width/2-280,height/2-60,140,40])    
+        else:
+            pygame.draw.rect(screen,button_dark,[width/2-280,height/2-60,140,40])
+    #code for making two player button lighter when hovered over it 
+        if width/2+140 <= mouse[0] <= width/2+280 and height/2-60 <= mouse[1] <= height/2-20:
+            pygame.draw.rect(screen,button_light,[width/2+140,height/2-60,140,40])    
+        else:
+            pygame.draw.rect(screen,button_dark,[width/2+140,height/2-60,140,40])
+    #code for making quit button lighter when hovered over it 
+        if width/2-70 <= mouse[0] <= width/2+70 and height/2+20 <= mouse[1] <= height/2+60:
+            pygame.draw.rect(screen,button_light,[width/2-70,height/2+20,140,40])        
+        else:
+            pygame.draw.rect(screen,button_dark,[width/2-70,height/2+20,140,40])
+      
+        screen.blit(quittext , (width/2-20,height/2+30))
+        screen.blit(onetext , (width/2-250,height/2-50))
+        screen.blit(twotext , (width/2+170,height/2-50))
+        pygame.display.update()  
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if width/2-70 <= mouse[0] <= width/2+70 and height/2+20 <= mouse[1] <= height/2+60:
+                    pygame.quit()
+                    sys.exit()
+            #What happens when the tetris button is clicked
+                if width/2-280 <= mouse[0] <= width/2-140 and height/2-60 <= mouse[1] <= height/2-20:
+                    running = False
+                    
+                    twoplayer()
+            #What happens when the space invaders button is clicked
+                if width/2+140 <= mouse[0] <= width/2+280 and height/2-60 <= mouse[1] <= height/2-20:
+                    running = False
+                    
+                    twoplayer()
+                    
+                  
+        
+        mouse = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if width-280 <= mouse[0] <= width-140 and height-80 <= mouse[1] <= height-40:
+                    pygame.quit()
+                    sys.exit()
+
         
 if __name__ == "__main__":
 	game()
